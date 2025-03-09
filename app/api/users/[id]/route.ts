a4a36ed0c240c2c45fd228552a44c43ebae312a4
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-09 12:49:56 +0530

    Fixed: Type "{ params: { id: string; }; }" is not a valid type for the function's second argument.

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,18 +1,21 @@
 import prisma  from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-  const user = await prisma.user.findUnique({ where: { id: params.id } });
+export async function GET(req: Request) {
+  const id = ""
+  const user = await prisma.user.findUnique({ where: { id: id } });
   return user ? NextResponse.json(user) : NextResponse.json({ error: "User not found" }, { status: 404 });
 }
 
-export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(req: Request) {
   const body = await req.json();
-  const updatedUser = await prisma.user.update({ where: { id: params.id }, data: body });
+  const id = ""
+  const updatedUser = await prisma.user.update({ where: { id: id }, data: body });
   return NextResponse.json(updatedUser);
 }
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
-  await prisma.user.delete({ where: { id: params.id } });
+export async function DELETE(req: Request) {
+  const id = ""
+  await prisma.user.delete({ where: { id: id } });
   return NextResponse.json({ message: "User deleted" });
 }
