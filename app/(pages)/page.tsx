8adeff3406056d8ce9b4c83dc0a95810c4b3c5ca
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-09 20:22:53 +0530

    Page removed from (pages)

--- app/(pages)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth";
-import HomePage from "../components/home";
-import LandingPage from "../components/landing-page";
-
-export default async function Page() {
-  const session = await auth(); 
-  if(!session || !session.user) return  <LandingPage/>
-  return <HomePage/>;
-}
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@/auth";
+import HomePage from "./components/home";
+import LandingPage from "./components/landing-page";
+import Footer from "./components/footer";
+
+export default async function Page() {
+  const session = await auth();
+  if (!session || !session.user) return <LandingPage />
+  return (
+    <>
+      <HomePage />
+      {session && <Footer />}
+    </>
+  );
+}
