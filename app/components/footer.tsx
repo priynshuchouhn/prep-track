c2a0600e37b3f34f8bce73ff1dbced00a328aef7
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-09 20:41:40 +0530

    Post Add UI added

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,20 @@
 import { auth } from "@/auth";
 import { Home, Search, PlusCircle, Bell, User } from "lucide-react";
 import Link from "next/link";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import CreatePostForm from "@/components/ui/create-post-form";
+
 
 async function Footer() {
     const session = await auth()
-    if(!session) return null;
+    if (!session) return null;
     return (
         <div className="bg-white fixed bottom-0 lg:hidden flex items-center justify-around p-3 w-full shadow-md shadow-gray-900 border-t border-gray-300">
             <Link href="/">
@@ -13,9 +23,20 @@ async function Footer() {
             <button>
                 <Search className="w-7 h-7 text-gray-700 hover:text-black" />
             </button>
-            <button className="">
-                <PlusCircle className="w-10 h-10 text-gray-700 hover:text-black" />
-            </button>
+            <Sheet>
+                <SheetTrigger>
+                    <PlusCircle className="w-10 h-10 text-gray-700 hover:text-black" />
+                </SheetTrigger>
+                <SheetContent side="bottom" className="h-full">
+                    <SheetHeader>
+                        <SheetTitle>Post an update</SheetTitle>
+                        <SheetDescription>
+                            <CreatePostForm rows={15} className="h-[80vh] overflow-y-auto"/>
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+
             <Link href="/">
                 <Bell className="w-7 h-7 text-gray-700 hover:text-black" />
             </Link>
--- components/ui/create-post-form.tsx
+++ components/ui/create-post-form.tsx
@@ -4,28 +4,29 @@ import { Card } from './card';
 import { Textarea } from './textarea';
 import { Button } from './button';
 import { BookMarked } from 'lucide-react';
+import { cn } from '@/lib/utils';
 
-function CreatePostForm() {
-    const [postContent, setPostContent] = useState("");
+function CreatePostForm({className, rows}: {className?:string, rows?: number}) {
+  const [postContent, setPostContent] = useState("");
   return (
-    <Card className="p-2 md:p-6 md:mb-6">
-              <Textarea
-              rows={8}
-                placeholder="Share your learning update..."
-                className="mb-4"
-                value={postContent}
-                onChange={(e) => setPostContent(e.target.value)}
-              />
-              <div className="flex justify-between items-center">
-                <div className="flex space-x-2">
-                  <Button variant="outline" size="sm">
-                    <BookMarked className="h-4 w-4 mr-2" />
-                    Topic
-                  </Button>
-                </div>
-                <Button>Post Update</Button>
-              </div>
-            </Card>
+    <Card className={cn("p-2 md:p-6 md:mb-6 flex flex-col justify-between", className)}>
+      <Textarea
+        rows={rows}
+        placeholder="Share your learning update..."
+        className="mb-4"
+        value={postContent}
+        onChange={(e) => setPostContent(e.target.value)}
+      />
+      <div className="flex justify-between items-center">
+        <div className="flex space-x-2">
+          <Button variant="outline" size="sm">
+            <BookMarked className="h-4 w-4 mr-2" />
+            Topic
+          </Button>
+        </div>
+        <Button>Post Update</Button>
+      </div>
+    </Card>
   )
 }
 
