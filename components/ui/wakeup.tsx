1878612706728a37ca7bbf7417b10607a36177c0
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-10 23:37:16 +0530

    App: "Only user can initialise ws"

--- components/ui/wakeup.tsx
+++ components/ui/wakeup.tsx
@@ -2,34 +2,38 @@
 import { useEffect, useState } from "react";
 import { initializeWebSocket, getSocket } from "@/lib/websocket";
 import { WS_BASE_URL } from "@/lib/utils";
+import { useSession } from "next-auth/react";
 
 
 export const WakeUpServer = () => {
     const [isWebSocketReady, setIsWebSocketReady] = useState(false);
+    const session = useSession();
 
     useEffect(() => {
         const wakeUpAndConnect = async () => {
-            try {
-                console.log("🚀 Waking up WebSocket server...");
-
-                console.log("🔌 Initializing WebSocket...");
-                initializeWebSocket(`${WS_BASE_URL}`, "");
-
-                const socket = getSocket();
-                socket.on("connect", () => {
-                    setIsWebSocketReady(true);
-                });
-
-                socket.on("disconnect", () => {
-                    setIsWebSocketReady(false);
-                });
-            } catch (error) {
-                console.error("⚠️ Failed to wake up WebSocket server:", error);
+            if(session.data?.user.id){
+                try {
+                    console.log("🚀 Waking up WebSocket server...");
+    
+                    console.log("🔌 Initializing WebSocket...");
+                    initializeWebSocket(`${WS_BASE_URL}`, session.data?.user.id);
+    
+                    const socket = getSocket();
+                    socket.on("connect", () => {
+                        setIsWebSocketReady(true);
+                    });
+    
+                    socket.on("disconnect", () => {
+                        setIsWebSocketReady(false);
+                    });
+                } catch (error) {
+                    console.error("⚠️ Failed to wake up WebSocket server:", error);
+                }
             }
         };
 
         wakeUpAndConnect();
-    }, []);
+    }, [session.data?.user.id]);
 
     return null;
 };
