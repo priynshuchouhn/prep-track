6e8f4e8181a161044bc70b11cb91767aca822bec
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-11 22:15:44 +0530

    App: "Post Metaddata added"

--- app/(pages)/posts/[slug]/page.tsx
+++ app/(pages)/posts/[slug]/page.tsx
@@ -1,11 +1,55 @@
+import PostCard from '@/components/ui/post-card'
+import { API_BASE_URL } from '@/lib/utils'
+import { Post, User } from '@prisma/client'
+import axios from 'axios'
+import { Metadata } from 'next'
 import React from 'react'
 
-function Page({params}: {params: Promise<{slug:string}>}) {
-  return (
-    <div>
-      
-    </div>
-  )
+type PostType = Post & { user: User}
+
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
+    const {slug} = await params
+    const res = await axios.get(`${API_BASE_URL}/posts/${slug}`)
+    const post: PostType  = res.data
+
+    return {
+        title: post.content,
+        description: post.content || 'Read this amazing post on our platform.',
+        keywords: post.tags ? post.tags.join(', ') : '',
+        authors: post.user.name ? [{ name: post.user.name }] : [],
+        publisher: 'Prep Track',
+        robots: 'index, follow',
+        alternates: {
+            canonical: `${API_BASE_URL}/posts/${slug}`,
+        },
+        openGraph: {
+            title: post.content,
+            description: post.content,
+            url: `${API_BASE_URL}/posts/${slug}`,
+            siteName: 'Your Website Name',
+            images: post.user.image ? [{ url: post.user.image, alt: post.content }] : [],
+            type: 'article',
+            publishedTime: `${post.createdAt}`,
+            modifiedTime: `${post.updatedAt}`,
+            authors: post.user.name ? [post.user.name] : [],
+            section: post.tags[0] || 'General',
+            tags: post.tags || []
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: post.content,
+            description: post.content,
+            images: post.user.image ? [post.user.image] : [],
+        },
+    }
+}
+
+async function Page({ params }: { params: Promise<{ slug: string }> }) {
+    const {slug} = await params
+    const res = await axios.get(`${API_BASE_URL}/posts/${slug}`)
+    const post = res.data
+    
+    return <PostCard post={post} />
 }
 
 export default Page
---
+++ app/api/posts/[slug]/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from '@/lib/prisma';
+import { revalidatePath } from "next/cache";
+
+export async function GET(req: NextRequest, {params}: {params: Promise<{slug:string}>}) {
+    try {
+        const {slug} = await params
+        const post = await prisma.post.findUnique({
+            where: {
+                slug
+            },
+            include: {
+                user: {
+                    select: {
+                        name: true,
+                        email: true,
+                        image: true,
+                        password: false
+                    }
+                },
+                Like:true,
+                Comment: true
+            }
+        });
+        revalidatePath('/posts/[slug]', 'page');
+        return NextResponse.json(post);
+    } catch (error) {
+        return NextResponse.json([]);
+    }
+}
