774f89ed37e9c90215ae0537b3e009c4f62aa4b7
Priyanshu Chouhan <priynshuchouhn@gmail.com>
2025-03-28 11:33:03 +0530

    App: "Minor UI Fixes on Home Page"

--- components/ui/hover-card.tsx
+++ components/ui/hover-card.tsx
@@ -1,5 +1,5 @@
 export const HoverCard = ({ children }: { children: React.ReactNode }) => {
     return (
-      <div className="group relative transition-all duration-500 hover:-translate-y-2 hover:shadow-xl">{children}</div>
+      <div className="group h-full relative transition-all duration-500 hover:-translate-y-2 hover:shadow-xl">{children}</div>
     )
   }
